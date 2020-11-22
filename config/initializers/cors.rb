7c9a18e9cd742907eb07a2354799bc27a37a7750
github username <constance.hyman@mchsi.com>
2020-11-22 12:03:31 -0800

    Change origin site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://this-happened.netlify.app/'
+    origins '*'
 
-    resource 'https://this-happened.netlify.app/',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
