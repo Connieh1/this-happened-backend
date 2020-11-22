a6fb33e0324ca82cf40d1f93673eab5ebdbc74b4
github username <constance.hyman@mchsi.com>
2020-11-21 23:16:30 -0800

    Update URL in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://this-happened-backend.herokuapp.com/'
 
-    resource '*',
+    resource 'https://this-happened-backend.herokuapp.com/',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
