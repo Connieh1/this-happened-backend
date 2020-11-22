4089910cb4574a65aea3f5a4d8a88d9eabf6fdc1
github username <constance.hyman@mchsi.com>
2020-11-22 11:37:01 -0800

    Prepare for deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://this-happened-backend.herokuapp.com/'
+    origins '*'
 
-    resource 'https://this-happened-backend.herokuapp.com/',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
